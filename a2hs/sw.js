5b37e7a0660cb3938f96ca97dfecb33cb1ac4773
Heydar Shams <shamsheidar.79@gmail.com>
2020-05-20 11:15:52 +0430

    init project

---
+++ a2hs/sw.js
@@ -0,0 +1,30 @@
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('video-store').then(function(cache) {
+     return cache.addAll([
+       '/a2hs/',
+       '/a2hs/index.html',
+       '/a2hs/index.js',
+       '/a2hs/style.css',
+       '/a2hs/images/fox1.jpg',
+       '/a2hs/images/fox2.jpg',
+       '/a2hs/images/fox3.jpg',
+       '/a2hs/images/fox4.jpg'
+     ]);
+   })
+ );
+});
+
+self.addEventListener('push', function(e) {
+  console.log('PUSH: ' + e.data);
+})
+
+
+self.addEventListener('fetch', function(e) {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
